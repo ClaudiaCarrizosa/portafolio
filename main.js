41a32c42d90900c4be3a43d73e1a4d73c03d4a7d
ClaudiaCarrizosa <140433673+ClaudiaCarrizosa@users.noreply.github.com>
2023-07-28 21:11:43 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+/* const myModal = document.getElementById('modal-contactame')
+const myElement = document.getElementById('contactame')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+}); */
+
+let app = document.getElementById('typewriter');
+
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+// 1000 milisegundos es 1 segundo
+  .pauseFor(1000)
+  .typeString('Andrea Perez')
+  .pauseFor(300)
+  .deleteChars(10)
+  .start();
